f960c7dad6771f5282d9672ae85f09a15e935f26
kedarjoyner <kedarjoyner@gmail.com>
2017-08-08 13:48:37 -0500

    display notifications message in nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NotificationsComponent, WarningComponent, SuccessComponent
   ],
   imports: [
     BrowserModule,
