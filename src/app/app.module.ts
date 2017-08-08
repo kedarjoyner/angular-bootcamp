02789a2c89c579e79daad03224fea47cf54677ef
kedarjoyner <kedarjoyner@gmail.com>
2017-08-08 12:19:51 -0500

    add comments to explain component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+// tells typescript to use ServerComponent
+import { ServerComponent } from './server/server.component';
+
+/* Angular uses components to build web pages 
+ * and uses modules to bundle different pieces 
+ * of the app into packages 
+*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ServerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+// components are a typescript class
+
+// needs decorator that tells Angular this is a component
+import { Component } from '@angular/core';
+
+// now need to configure Component to tell Angular what to do with it
+@Component({
+    // html tag needed to use Component in other templates
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
