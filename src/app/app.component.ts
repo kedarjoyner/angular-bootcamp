136ff8030a1b7a5e4104cb3172a1672d06d069d8
kedarjoyner <kedarjoyner@gmail.com>
2017-08-08 13:04:48 -0500

    different ways to style selectors, make selector into attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // can add array of style files
+  // styleUrls: ['./app.component.css']
+  // can also add styles inline
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
 
--- src/app/kedar-components/kedar-components.component.ts
+++ src/app/kedar-components/kedar-components.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-kedar-components',
+  // makes selector attribute that can be used
+  selector: '[app-kedar-components]',
   templateUrl: './kedar-components.component.html',
   styleUrls: ['./kedar-components.component.css']
 })
