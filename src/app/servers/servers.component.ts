a625d2ce972ddbd37debb0355252c3a8182be97c
kedarjoyner <kedarjoyner@gmail.com>
2017-08-08 21:23:36 -0500

    constructor definition, demonstrate event and property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,15 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
   constructor() {
-  /* A constructor is a method executed at the point 
-   * of time this component is created by Angular 
+  //called first time before the ngOnInit()
+  /*  Normally we use constructor to define/initialize some variables and dependency injection, 
+   *  but when we have tasks related to Angular's bindings we move to Angular's 
+   * ngOnInit life cycle hook.
   */
+
   setTimeout(() => {
     this.allowNewServer = true;
   }, 2000);
 }
   ngOnInit() {
+     //called after the constructor and called  after the first ngOnChanges() 
+  }
+
+  // method
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
