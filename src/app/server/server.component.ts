49dadb860460533fec8873cfe66e1fc9de5a9a93
kedarjoyner <kedarjoyner@gmail.com>
2017-08-08 20:28:32 -0500

    Demonstrates diff types of string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,5 +11,11 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 
 }
